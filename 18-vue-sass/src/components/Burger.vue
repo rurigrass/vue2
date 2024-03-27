a4f7eb22dbe4a625a3c59550d46844d50674f236
Ruairidh Grass <ruripg@gmail.com>
2024-03-27 16:25:29 +0100

    added checkbox trick

--- 18-vue-sass/src/components/Burger.vue
+++ 18-vue-sass/src/components/Burger.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="burger">
+    <input type="checkbox" class="burger__checkbox">
     <span class="burger__side">TEXT</span>
     <div class="line line-1"></div>
     <div class="line line-2"></div>
