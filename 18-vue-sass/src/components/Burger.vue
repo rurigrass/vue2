30ae10c6aa83b14c41af6f312ca7f66765dc2b57
Ruairidh Grass <ruripg@gmail.com>
2024-03-27 14:35:36 +0100

    burger animation on hover

--- 18-vue-sass/src/components/Burger.vue
+++ 18-vue-sass/src/components/Burger.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="c-header__burger">
-    <div></div>
-    <div></div>
-    <div></div>
+  <div class="burger">
+    <span class="burger__side">TEXT</span>
+    <div class="line line-1"></div>
+    <div class="line line-2"></div>
+    <div class="line line-3"></div>
   </div>
 </template>
 
