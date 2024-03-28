d39466900747da77a5d1c5ba11f78fd1de0f82bf
Ruairidh Grass <ruripg@gmail.com>
2024-03-28 14:44:52 +0100

    rotate burger on click

--- 18-vue-sass/src/components/Burger.vue
+++ 18-vue-sass/src/components/Burger.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="burger">
-    <input type="checkbox" class="burger__checkbox">
-    <span class="burger__side">TEXT</span>
-    <div class="line line-1"></div>
-    <div class="line line-2"></div>
-    <div class="line line-3"></div>
-  </div>
+    <div class="burger" :class="{ 'burger__checked': checked }" @click="checked = !checked">
+      <!-- <input type="checkbox" id="burger__checkbox" @change="check">
+        <label class="burger__side" for="burger__checkbox">TEXT</label> -->
+        <div class="burger__side burger__side--front">
+          <div class="line line-1"></div>
+          <div class="line line-2"></div>
+          <div class="line line-3"></div>
+        </div>
+        <div class="burger__side burger__side--back">
+          <div class="line line-4"></div>
+          <div class="line line-5"></div>
+        </div>
+      
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+      return {checked: false} 
+    },
+    props: {
+      check: Boolean
+    }
+};
 </script>
 
-<style></style>
+<style>
+
+</style>
