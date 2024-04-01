21f0ccb24ce7a55a0da8f5c6f3af6357553d3a39
Ruairidh Grass <ruripg@gmail.com>
2024-04-01 18:04:33 +0100

    last column has addjustable width

--- 18-vue-sass/src/components/Background.vue
+++ 18-vue-sass/src/components/Background.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="c-background">
-        <div v-for="col in Math.floor(screenWidth / 40)">
+        <div v-for="col in Math.ceil(screenWidth / 40)" :style="{ '--last-width': (screenWidth / 40) % 1  + 'px'}">
             {{ col }}
         </div>
     </div>
@@ -12,7 +12,9 @@ export default {
         screenWidth: Number
     },
     mounted() {
-        console.log(this.screenWidth / 40);
+        console.log(Math.ceil(this.screenWidth / 40));
+
+        console.log("last ", (this.screenWidth / 40) % 1 * 40);
     }
 }
 </script>
