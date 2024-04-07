1efbe3d5eb1f9ae3810bd0e37b29f53e366c3b85
Ruairidh Grass <ruripg@gmail.com>
2024-04-07 02:00:23 +0100

    hover background cube

--- 18-vue-sass/src/App.vue
+++ 18-vue-sass/src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
     <Header></Header>
-    <Background :screenWidth="innerWidth" :dimensions="dimensions"></Background>
     <Menu v-show="$store.state.menuOpen"></Menu>
+    <Background :screenWidth="innerWidth" :dimensions="dimensions"></Background>
     <div class="container">
       <InvertedText class="container__left container__center container__ml text__lg" :text-fill="innerWidth / 3 - 102"
-        :extra="innerWidth" fill="__black-left">Cascade</InvertedText>
+      :extra="innerWidth" fill="__black-left">Cascade</InvertedText>
       <ImageContainer class="container__right" link="/src/assets/dmitriy-galanov.jpg" />
     </div>
     <div :style="{ padding: '20px' }"></div>
     <div class="container">
       <ImageContainer class="container__left" link="/src/assets/sava-bobov.jpg" />
       <InvertedText class="container__right container__mt text__sm" :text-fill="innerWidth / 6 - 5.5"
-        fill="__white-left"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem illum
-        aut dolore esse maiores ullam, fugiat temporibus aliquid alias itaque
-        nulla amet id deserunt natus expedita! Facere possimus sequi velit!
-      </InvertedText>
-    </div>
+      fill="__white-left"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem illum
+      aut dolore esse maiores ullam, fugiat temporibus aliquid alias itaque
+      nulla amet id deserunt natus expedita! Facere possimus sequi velit!
+    </InvertedText>
+  </div>
   </div>
 </template>
 
