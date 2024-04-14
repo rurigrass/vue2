2d00497e33c98fd1204ddafbbe5b24e4ec270221
Ruairidh Grass <ruripg@gmail.com>
2024-04-14 22:20:12 +0100

    background component compose

--- 18-vue-sass/src/components/Background.vue
+++ 18-vue-sass/src/components/Background.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-
-    </div>
+   <div class="c-background">
+            <div v-for="(col, i) in Math.ceil(dimensions.page.width / 40)" :key="i" class="c-background__column"
+                :style="{ '--last-width': lastWidth + 'px' }">
+                <div v-for="(row, j) in getRows" :key="j" class="c-background__row">
+                        <div class="c-background__block"></div>
+                </div>
+            </div>
+        </div>
 </template>
 
 <script>
